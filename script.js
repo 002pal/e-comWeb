03319591194c1ebd690eacbb67698f92315d157a
Mr_Pal <94923357+002pal@users.noreply.github.com>
2024-09-05 19:54:30 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+//===== search button function =====
+let searchBtn = document.getElementById('searchBtn');
+
+searchBtn.addEventListener('click', function() {
+    alert('this event not work!');
+});
