5e705481279c12fa7bc9775c507372b8bebceaaa
Mr_Pal <94923357+002pal@users.noreply.github.com>
2024-10-08 01:07:04 +0530

    other js file

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-//===== search button function =====
-let searchBtn = document.getElementById('searchBtn');
+//===== user profile dropdown function =====
+let userDrop_menu = document.getElementById('user-dropdown-menu');
+let dropdown_menu = document.getElementById('custom-profile-icon');
 
-searchBtn.addEventListener('click', function() {
-    alert('this event not work!');
+userDrop_menu.addEventListener('click', () => {
+    dropdown_menu.classList.toggle('hover');
+});
+
+//===== first purchase section popup function with delay using jquery =====
+// $(window).on('load', function() {
+//     setTimeout(function() {
+//         $('#popupModal').modal('show');
+//     }, 2000);
+// });
+
+(function () {
+    setTimeout(function() {
+        $('#popupModal').modal('show');
+    }, 2000);
+})();
+
+//===== cart add function =====
+const cart = document.querySelectorAll('#cart_add');
+const mycart_area = document.getElementById('mycart-area')
+
+cart.forEach(e => {
+    e.addEventListener('click', function() {
+        alert('cart not add to "My Cart Page" we will fixed soon');
+    });
 });
